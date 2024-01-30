17860e8e5d78a61f638e1d69761a46ee1e98e6e1
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2024-01-30 21:14:29 +0600

    task_profile

---
+++ Backend/Migrations/20240130151425_task_profile.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class task_profile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProfileId",
+                table: "Tasks_T",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_T_ProfileId",
+                table: "Tasks_T",
+                column: "ProfileId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_T_Profiles_T_ProfileId",
+                table: "Tasks_T",
+                column: "ProfileId",
+                principalTable: "Profiles_T",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_T_Profiles_T_ProfileId",
+                table: "Tasks_T");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tasks_T_ProfileId",
+                table: "Tasks_T");
+
+            migrationBuilder.DropColumn(
+                name: "ProfileId",
+                table: "Tasks_T");
+        }
+    }
+}
